8c8cf5aa8458dd3d81a8d3643175585b59804ae5
John Crowley <john@sightsource.net>
2023-03-26 14:27:09 -0400

    container layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import { useEffect, useState } from "react";
 import Editor from "./components/Editor";
 import TokenInspector from "./components/TokenInspector";
 import Toolbar from "./components/Toolbar";
-import fuzzySearch from "./lib/fuzzySearch";
 import { ThemeProvider } from "./lib/hooks/useTheme";
 import { jld, sql } from "./lib/initValue";
 import * as jsonLd from "./lib/languages/json-ld";
 import { LanguageDefinition } from "./lib/languages/types";
-import { Token } from "./lib/tokenize";
-import { testSuggestions } from "./testSuggestions";
 
 function App() {
   const [value, setValue] = useState("");
@@ -27,55 +24,57 @@ function App() {
   }
 
   return (
-    <ThemeProvider>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "baseline",
-          padding: "1.5rem 3rem 0 3rem",
-        }}
-      >
-        <h2>Johnaco Editor </h2>&nbsp;&nbsp;
-        <a
-          href="https://github.com/johncrowley-s2/johnaco-editor"
-          target="_blank"
+    <div style={{ maxWidth: "1100px", margin: "0 auto" }}>
+      <ThemeProvider>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "baseline",
+            padding: "1.5rem 3rem 0 3rem",
+          }}
         >
-          View code on Github
-        </a>
-      </div>
-      <div
-        style={{
-          height: "30rem",
-          padding: "1rem 3rem",
-        }}
-      >
-        <Editor
-          readonly={readonly}
-          highlight={highlight}
-          language={language}
-          value={value}
-          onValueChange={(x) => setValue(x)}
-          showLineNumbers={showLineNumbers}
-        />
-      </div>
-      <div style={{ padding: "0 3rem" }}>
-        <Toolbar
-          showLineNumbers={showLineNumbers}
-          setShowLineNumbers={setShowLineNumbers}
-          highlight={highlight}
-          setHighlight={setHighlight}
-          readonly={readonly}
-          setReadonly={setReadonly}
-          language={language}
-          setLanguage={setLanguage}
-          prettify={prettify}
-        />
-        {/* <hr /> */}
-        {/* <Errors numLines={numLines} numTokens={tokens.length} errors={errors} /> */}
-        {/* <hr /> */}
-        <TokenInspector value={value} language={language} />
-      </div>
-    </ThemeProvider>
+          <h2>Johnaco Editor </h2>&nbsp;&nbsp;
+          <a
+            href="https://github.com/johncrowley-s2/johnaco-editor"
+            target="_blank"
+          >
+            View code on Github
+          </a>
+        </div>
+        <div
+          style={{
+            height: "30rem",
+            padding: "1rem 3rem",
+          }}
+        >
+          <Editor
+            readonly={readonly}
+            highlight={highlight}
+            language={language}
+            value={value}
+            onValueChange={(x) => setValue(x)}
+            showLineNumbers={showLineNumbers}
+          />
+        </div>
+        <div style={{ padding: "0 3rem" }}>
+          <Toolbar
+            showLineNumbers={showLineNumbers}
+            setShowLineNumbers={setShowLineNumbers}
+            highlight={highlight}
+            setHighlight={setHighlight}
+            readonly={readonly}
+            setReadonly={setReadonly}
+            language={language}
+            setLanguage={setLanguage}
+            prettify={prettify}
+          />
+          {/* <hr /> */}
+          {/* <Errors numLines={numLines} numTokens={tokens.length} errors={errors} /> */}
+          {/* <hr /> */}
+          <TokenInspector value={value} language={language} />
+        </div>
+      </ThemeProvider>
+    </div>
   );
 }
 
